a707de19f88ab3e7b69f4197c213d3a3cbfae3fa
BARTHE <barthemarc@yahoo.fr>
2020-03-07 13:29:18 +0000

    Ajout fichier objet

---
+++ objet.js
@@ -0,0 +1,10 @@
+// let user={
+//     name:"marc",
+//     prenom:"philippe",
+//     age:45
+// }
+// console.log(user.age)
+// console.log(user)
+mailpipo='barthemarc@yahoo.fr'
+validator=require('validator')
+console.log(validator.isEmail(mailpipo));
